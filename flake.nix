e67dfc6675d0f285aed7f56519025f231e2413a5
drzzln <drzzln@protonmail.com>
2026-03-06 16:00:18 -0500

    feat: add devenv input for enhanced developer environments

--- flake.nix
+++ flake.nix
@@ -7,15 +7,39 @@
       url = "github:pleme-io/substrate";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     substrate,
-  }: {
+    devenv,
+  }:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"
+    ];
+  in {
     homeManagerModules.default = import ./module {
       hmHelpers = import "${substrate}/lib/hm-service-helpers.nix" {lib = nixpkgs.lib;};
     };
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = devenv.lib.mkShell {
+        inputs = { inherit nixpkgs devenv; };
+        inherit pkgs;
+        modules = [{
+          languages.nix.enable = true;
+          packages = with pkgs; [ nixpkgs-fmt nil ];
+          git-hooks.hooks.nixpkgs-fmt.enable = true;
+        }];
+      };
+    });
   };
 }
